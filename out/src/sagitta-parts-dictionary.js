1b9d52121be4ab1fc658420bc18e12165c3296be
Martin Calsyn <mcalsyn@pervasive.digital>
2017-02-27 12:38:33 -0800

    Need to publish these ... as CI does not generate them and I don't want to require typescript on client machines

---
+++ out/src/sagitta-parts-dictionary.js
@@ -0,0 +1,9 @@
+"use strict";
+class SagittaPartsLibrary {
+    generatePartsLibrary() {
+    }
+    insertOrUpdate(path) {
+    }
+}
+exports.SagittaPartsLibrary = SagittaPartsLibrary;
+//# sourceMappingURL=sagitta-parts-dictionary.js.map
