ccde652afc322ef86e8ffd72ac100bd8dad1bb81
Martin Calsyn <mcalsyn@pervasive.digital>
2017-02-27 16:36:37 -0800

    Create an empty db

--- out/src/sagitta-parts-dictionary.js
+++ out/src/sagitta-parts-dictionary.js
@@ -1,8 +1,29 @@
 "use strict";
+const sql = require("sqlite3");
+const os = require("os");
+const path = require("path");
+const mkdirp = require("mkdirp");
 class SagittaPartsLibrary {
+    constructor() {
+        this.openDb();
+    }
     generatePartsLibrary() {
     }
     insertOrUpdate(path) {
+    }
+    partDescriptionIsValid(path) {
+        return true;
+    }
+    openDb() {
+        if (this.db)
+            throw new Error("Db already open");
+        let filepath = os.homedir();
+        if (process.platform === 'win32')
+            filepath = path.join(filepath, 'Documents');
+        filepath = path.join(filepath, "Sagitta", "Designer", "PartsLibrary");
+        mkdirp(filepath);
+        filepath = path.join(filepath, "partslibrary.db");
+        this.db = new sql.Database(filepath);
     }
 }
 exports.SagittaPartsLibrary = SagittaPartsLibrary;
--- src/sagitta-parts-dictionary.ts
+++ src/sagitta-parts-dictionary.ts
@@ -1,7 +1,16 @@
+import * as sql from 'sqlite3';
+import os = require('os');
+import path = require('path');
+import mkdirp = require('mkdirp');
 
 class SagittaPartsLibrary {
+    private db : sql.Database;
+
+    constructor() {
+        this.openDb();
+    }
+
     generatePartsLibrary() {
-        
     }
 
     insertOrUpdate(path : string) {
@@ -10,6 +19,20 @@ class SagittaPartsLibrary {
 
     partDescriptionIsValid(path : string) : boolean {
         return true;
+    }
+
+    openDb()
+    {
+        if (this.db)
+            throw new Error("Db already open");
+
+        let filepath = os.homedir();
+       	if (process.platform === 'win32')
+		    filepath = path.join(filepath, 'Documents'); 
+        filepath = path.join(filepath, "Sagitta", "Designer", "PartsLibrary");
+        mkdirp(filepath);
+        filepath = path.join(filepath,"partslibrary.db");
+        this.db = new sql.Database(filepath)
     }
 }
 
